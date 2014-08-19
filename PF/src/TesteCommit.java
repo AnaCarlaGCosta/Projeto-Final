6c69d464bb28287996cad3911ae2cec8f6f7f8b0
bruno <bruno@bruno-PC>
2014-08-19 16:39:04 -0300

    commit teste1

--- PF/src/TesteCommit.java
+++ PF/src/TesteCommit.java
@@ -9,7 +9,13 @@
  */
 public class TesteCommit {
     
-    int x;
-    private String teste;
+    private String teste1;
+    private String teste2;
+    private String teste3;
+    private String teste4;
     
+    private String teste5;
+    
+    
+    int e;
 }
